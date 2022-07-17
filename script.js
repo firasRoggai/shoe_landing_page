b83174a259dfb04a707566e5720ae539f5a99e36
firasRoggai <firasreggai@gmail.com>
2022-07-17 22:51:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+/***  VARIABLES ***/
+const btn1 = document.querySelector('.btn-1')
+const btn2 = document.querySelector('.btn-2')
+const btn3 = document.querySelector('.btn-3')
+const btn4 = document.querySelector('.btn-4')
+const cont = document.querySelector('.cards-container')
+const cont1 = document.querySelector('.cards-container-1')
+const cont2 = document.querySelector('.cards-container-2')
+const cont3 = document.querySelector('.cards-container-3')
+const cont4 = document.querySelector('.cards-container-4')
+/*** END VARIABLES ***/
+btn1.addEventListener('click',handleClick)
+btn2.addEventListener('click',handleClick)
+btn3.addEventListener('click',handleClick)
+btn4.addEventListener('click',handleClick)
+
+function handleClick(event){
+    switchContent(event.target.classList[1][4]) //passing the number of the btn clicked as an identifier
+}
+function switchContent(option){
+    let trans = `cont${Number(option)}`;
+    trans.classList.add('animate__fadeIn')
+    setTimeout(() => {
+        trans.classList.remove('animate__fadeIn')
+    }, 1000)
+    switch (option) {
+        case '1':
+            cont1.classList.remove('d-none')
+            cont2.classList.add('d-none')
+            cont3.classList.add('d-none')
+            cont4.classList.add('d-none')
+            break;
+        case '2':
+            cont2.classList.remove('d-none')
+            cont1.classList.add('d-none')
+            cont3.classList.add('d-none')
+            cont4.classList.add('d-none')
+            break;
+        case '3':
+            cont3.classList.remove('d-none')
+            cont1.classList.add('d-none')
+            cont2.classList.add('d-none')
+            cont4.classList.add('d-none')
+            break;
+        case '4':
+            cont4.classList.remove('d-none')
+            cont1.classList.add('d-none')
+            cont3.classList.add('d-none')
+            cont2.classList.add('d-none')
+            break;
+    
+        default:
+            break;
+    }
+}
